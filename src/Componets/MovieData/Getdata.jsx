5972ac6765a8db0dbdfb5d176089ed906cd0ccf3
Kishor Sarkar <kishor.sarkar@inadev.com>
2023-02-04 20:40:47 +0530

    add custom hooks

--- src/Componets/MovieData/Getdata.jsx
+++ src/Componets/MovieData/Getdata.jsx
@@ -1,47 +1,59 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import useHttp from "../../Hooks/httpHooks";
 import ShowList from "./ShowList";
 
 const Getdata = () => {
-  const [movieList, setMovieList] = useState(null);
+    const [movieList, setMovieList] = useState(null);
 
-  const GetDataFromFirebase = async () => {
-    const res = await fetch(
-      "https://react-tutorials-be6d1-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json",
-      {
-        method: "GET",
+    //   const GetDataFromFirebase = async () => {
+    //     const res = await fetch(
+    //       "https://react-tutorials-be6d1-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json",
+    //       {
+    //         method: "GET",
 
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
+    //         headers: {
+    //           "Content-Type": "application/json",
+    //         },
+    //       }
+    //     );
+
+    //     const data = await res.json();
+    //     console.log(Object.entries(data));
+    //     setMovieList(await Object.entries(data));
+    //   };
+
+    const param = {
+        endPoint: 'movies.json', method: 'get'
+    }
+
+
+    const { data, error, loading, sendRequest } = useHttp(param)
+
+    useEffect(() => {
+        data && setMovieList(Object.entries(data))
+    }, [data]);
+
+    return (
+        <div className="Getdata bg-light py-3">
+            <div className="row m-auto col-6 g-3">
+                <div className="col-12 m-auto">
+                    <button
+                        type="button"
+                        name=""
+                        onClick={sendRequest}
+                        id=""
+                        className="btn btn-primary btn-lg btn-block">
+                        Fetch Movie
+                    </button>
+                </div>
+                {error ? <div className="error">{error}</div> : <div className="col-12 m-auto">
+                    {movieList?.map((item) => (
+                        <ShowList key={item["0"]} item={item["1"]}></ShowList>
+                    ))}
+                </div>}
+            </div>
+        </div>
     );
-
-    const data = await res.json();
-    console.log(Object.entries(data));
-    setMovieList(await Object.entries(data));
-  };
-
-  return (
-    <div className="Getdata bg-light py-3">
-      <div className="row m-auto col-6 g-3">
-        <div className="col-12 m-auto">
-          <button
-            type="button"
-            name=""
-            onClick={GetDataFromFirebase}
-            id=""
-            className="btn btn-primary btn-lg btn-block">
-            Fetch Movie
-          </button>
-        </div>
-        <div className="col-12 m-auto">
-          {movieList?.map((item) => (
-            <ShowList key={item["0"]} item={item["1"]}></ShowList>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
 };
 
 export default Getdata;

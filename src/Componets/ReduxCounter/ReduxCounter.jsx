b024d8f0f560c89a971df1826d7eff1ab8757cbe
Kishor Sarkar <kishor.sarkar@inadev.com>
2023-02-11 22:38:01 +0530

    redux with multiple package

--- src/Componets/ReduxCounter/ReduxCounter.jsx
+++ src/Componets/ReduxCounter/ReduxCounter.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { counterActions } from "../../store";
+
+
 
 const ReduxCounter = () => {
-  const counter = useSelector((state) => state.count);
-  const show = useSelector((state)=>state.showCounter)
+
+  const counter = useSelector((state) => state.counter.count);
+  const show = useSelector((state)=>state.counter.showCounter);
+  console.log(show);
   const dispatch = useDispatch();
   return (
     <div className="ReduxCounter">
@@ -16,22 +21,22 @@ const ReduxCounter = () => {
           </p>
           <button
             className="btn btn-primary"
-            onClick={() => dispatch({ type: "inc" })}>
+            onClick={() => dispatch(counterActions.increment())}>
             Increase
           </button>
           <button
             className="btn btn-primary"
-            onClick={() => dispatch({ type: "incr",amount:5 })}>
+            onClick={() => dispatch(counterActions.increase(10))}>
             Increase 5
           </button>
           <button
-            onClick={() => dispatch({ type: "dec" })}
+            onClick={() => dispatch(counterActions.decrement())}
             className="btn btn-warning">
             Decress
           </button>
         </div>}
       </div>
-      <button  onClick={() => dispatch({ type: "toggle" })} className="btn btn-primary" type="button" >
+      <button  onClick={() => dispatch(counterActions.toggleBtn())} className="btn btn-primary" type="button" >
    Toggle Counter
   </button>
     </div>
